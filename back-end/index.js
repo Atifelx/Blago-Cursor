ff8c03d95ef38266739083b1b511ba5dfd281296
Atifelx <155761130+Atifelx@users.noreply.github.com>
2024-09-19 16:58:00 +0530

    Api has setup

--- back-end/index.js
+++ back-end/index.js
@@ -23,3 +23,11 @@ mongoose.connect(process.env.MONGOURI)
   .catch(err => console.error('MongoDB connection error:', err));
 
   
+  app.get('/test',(req,res)=>{
+
+    res.send({message:'Api is working in index js code!'});
+});
+
+app.listen(PORT ,() => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
